55b1f41e38b7236c47c3da043b9a121ab0304705
Nihar Garg <garg29@purdue.edu>
2015-10-13 21:38:27 -0400

    Test main.js func(button)

--- main.js
+++ main.js
@@ -63,3 +63,9 @@ function handleContactSubmission() {
     win.focus();
   });
 }
+$('.button').click(function() {
+  $('html, body').animate({
+      scrollTop: $(".home-main").offset().top
+    }, 700);
+  return false;
+});

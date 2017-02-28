a0b0436e2ee1fe90f4db9b685a4d1090c2023eb0
Nihar Garg <iamnihargarg@gmail.com>
2017-02-27 21:03:42 -0500

    added contact form, cleaned code, updated js and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,33 @@
-/*
- * This is a shorthand for $(document).ready(function() {...}). Anything that we
- * put inside of the two curly braces will get executed when the page has
- * finished loading. We do this because it takes time for the page (more
- * specifically the DOM) to load, and our functions that we call inside it
- * (registerNavButtons and handleContactSubmission) interact with the DOM.
- */
+// SMOOTH SCROLL
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
+// ******************************
+
+// ENABLE SUBMIT BUTTON WHEN VERIFICATION IS PASSED
 function enableContact() {
   $('#contact-button').removeAttr("disabled");
 }
 
-$(function() {
-  registerNavButtons();
-  handleContactSubmission();
-});
+// ******************************
 
+// CONTACT SUBMISSION BACKUP
 /* 
- * Here we declare a function that will call clickAndScroll for each of our 
- * three main sections on the homepage. We pass into clickAndScroll the id of
- * the link at the top of the page, and the id of the cooresponding section.
- */
-function registerNavButtons() {
-  var sections = ['about', 'writing', 'contact'];
-
-  sections.forEach(function(section) {
-    clickAndScroll('#nav-' + section, '#' + section + '-section');
-  });
-}
-
-/*
- * clickAndScroll is a function that lets us have a nice scrolling animating
- * when a user clicks one of the three links at the top of our homepage. It
- * accomplishes this by registering a click listener on a link and then when
- * that link is clicked it scrolls the entire page down to the correct section.
- */
-function clickAndScroll(link, section) {
-  var delay = 700;
-
-  $(link).click(function() {
-    $('html, body').animate({
-      scrollTop: $(section).offset().top
-    }, delay);
-
-    return false;
-  });
-}
-
-/*
- * handleContactSubmission lets us open the user's default mail client with a
- * new email to ourselves with the information entered into the form. First we
- * register a submit listener on the contact form, then we prevent the default
- * action from occurring (refreshing the page). From there we can pull out the
- * information in the form. With this information we can create a url that will
- * redirect the user (in a new tab) to their mail client with the form info.
- */
+$(function() {
+	handleContactSubmission();
+});
 function handleContactSubmission() {
   $('form').submit(function(e) {
     e.preventDefault();
@@ -61,44 +35,30 @@ function handleContactSubmission() {
     var last = $('input[name=last]').val();
     var subject = $('input[name=subject]').val();
     var message = $('textarea[name=message]').val();
-
+ 
     var url = 'mailto:nihar@purdue.edu?&subject=' + first + ' ' + last + ': ' + subject + '&body=' + message;
-
+ 
     var win = window.open(url, '_blank');
     win.focus();
   });
 }
-$('.button').click(function() {
-  $('html, body').animate({
-      scrollTop: $(".home-main").offset().top
-    }, 700);
-  return false;
+*/
+
+// ******************************
+
+// HOVER TEXT IN HEADER
+function helloEnter(x) {
+  x.textContent = "Nihar Garg."
+}
+function helloLeave(x) {
+  x.textContent = "My name is Garg,"
+}
+
+// SCROLL TEXT IN HEADER
+$(function(){
+  $(".scrollText").typed({
+    strings: ["Mechanical Engineer...", "Problem Solver...", "Tech Enthusiast..."],
+    loop: true,
+    typeSpeed: 0
+  });
 });
-
-
-
-
-//       (function(){
-//         // Add smooth scrolling to all links
-//         $("a").on('click', function(event) {
-
-//         // Make sure this.hash has a value before overriding default behavior
-//         if (this.hash !== "") {
-//           // Prevent default anchor click behavior
-//             event.preventDefault();
-
-//             // Store hash
-//             var hash = this.hash;
-
-//             // Using jQuery's animate() method to add smooth page scroll
-//             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//             $('html, body').animate({
-//             scrollTop: $(hash).offset().top
-//         }, 800, function(){
-   
-//             // Add hash (#) to URL when done scrolling (default click behavior)
-//             window.location.hash = hash;
-//             });
-//           }
-//         });
-//       };
---
+++ assets/php/contact.php
@@ -0,0 +1,13 @@
+<?php
+if ($_POST["submit"]) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$to = 'iamnihargarg@outlook.com';
+	$subject = 'Website Contact Form';
+
+	$body = "From: $name ($email)\n\n Message:\n$message";
+
+    mail ($to, $subject, $body, "From: $name <$email>");
+}
+?>

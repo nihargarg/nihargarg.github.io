943e7ffb2af077a2acecd450ce25ee5517003090
Nihar Garg <iamnihargarg@gmail.com>
2017-09-24 10:11:11 -0400

    testing 7

--- assets/js/sentimentalanalysis.js
+++ assets/js/sentimentalanalysis.js
@@ -3,9 +3,7 @@ $(function () {
 	$("#analyze-button").click(function(){
         var subscriptionKey = "a46c7301d6344c24b1b3b7c3659a38cf";
         var textToAnalyze = $("#message").val();
-
         var webSvcUrl = "https://eastus2.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment";
-
 
         var outputDiv = $("#OutputDiv");
         outputDiv.text("Thinking...");
@@ -16,13 +14,14 @@ $(function () {
             headers: { "Ocp-Apim-Subscription-Key": subscriptionKey },
             contentType: "application/json",
             data: '{"documents": [ { "language": "en", "id": "text01",  "text": "'+ textToAnalyze + '" }]}'
-        }).done(function (data) {
+        })
+        .done(function (data) {
 			if (data.errors.length > 0) {
                 outputDiv.html("Error: " + data.errors[0]);
 			}
             else if (data.documents.length > 0) {
 				var score = data.documents[0].score;
-				if (score > 0.5){
+				if (score >= 0.5){
 					outputText = "Thanks for yor positive message!"
 						+ "<br>"
 						+ "Score=" 
@@ -45,15 +44,11 @@ $(function () {
 				$("#PositiveImage").css("display", "none");
 				$("#NegativeImage").css("display", "none");
             }
-
-        }).fail(function (err) {
-            $("#OutputDiv").text("ERROR! " + err.responseText);
+		})
+		.fail(function (err) {
+            $("#OutputDiv").text("No text to analyze. Error code: " + err.responseText);
 			$("#PositiveImage").css("display", "none");
 			$("#NegativeImage").css("display", "none");
-        });
-		
-   });
-
-
+		});	
+  	});
 });
-

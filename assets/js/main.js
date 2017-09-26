a30972510919c74bf1a4474f508e7440bbee9fa2
Nihar Garg <iamnihargarg@gmail.com>
2017-09-26 18:45:19 -0400

    testing social media widgets 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,7 @@ function enableContact() {
 
 // ******************************
 
+//Twitter Widget
 window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
     t = window.twttr || {};
@@ -66,6 +67,15 @@ window.twttr = (function(d, s, id) {
 
   return t;
 }(document, "script", "twitter-wjs"));
+
+//Facebook Widget
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 // ******************************
 
